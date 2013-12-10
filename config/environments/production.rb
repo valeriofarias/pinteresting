221a1cf575d8b2e52d182cd47518c0d5bcc54f1e
valeriofarias <valeriofc@gmail.com>
2013-12-10 12:10:27 -0300

    install devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Pinteresting::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # Devise Requirement
+  config.action_mailer.default_url_options = { :host => 'http://valpinter.herokuapp.com' }
 end
